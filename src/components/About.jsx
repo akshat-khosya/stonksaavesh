d627458f45f38fd5eb25386cca56ef17cc97f1e2
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2022-01-11 10:43:03 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,23 @@ const About = () => {
             <div className="about-card">
                 <div className="about-card-heading">What is Stock Market</div>
                 <div className="about-card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in augue at est egestas cursus eu condimentum est. Donec auctor et diam nec porttitor. Nunc pulvinar lorem a tellus dictum, eget tempor sapien posuere. In sit amet orci quis ligula pellentesque faucibus.
+                   A stock market, equity market, or share market is the aggregation of buyers and sellers of stocks (also called shares), which represent ownership claims on businesses; these may include securities listed on a public stock exchange, as well as stock that is only traded privately, such as shares of private companies which are sold to investors through equity crowdfunding platforms. Investment in the stock market is most often done via stockbrokerages and electronic trading platforms. Investment is usually made with an investment strategy in mind.  
                 </div>
             </div>
             <div className="about-card">
                 <div className="about-card-heading">About Stonks</div>
                 <div className="about-card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in augue at est egestas cursus eu condimentum est. Donec auctor et diam nec porttitor. Nunc pulvinar lorem a tellus dictum, eget tempor sapien posuere. In sit amet orci quis ligula pellentesque faucibus.
+                   Ever thought of investing in the stock market but never tried
+                    because of no experience? If so, we have brought this event
+                    for you, where you can pick stocks and create your very own
+                    portfolio with the virtual money we provide you. Compete
+                    among other minds and create the best portfolio which
+                    provides the maximum return and wins the prize money.
+                    In our event, we will provide you a platform to invest in some
+                    NSE stocks and create your portfolio which will be analyzed
+                    by us.
+                    We will be searching for the best portfolio with the maximum
+                    return.
                 </div>
             </div>
             <div className="about-card">
